dd8ad27c8741e742a575d91eacca96b9c8f086b1
Björn Ångbäck <bjorn@angback.se>
2021-12-20 13:37:21 +0100

    prova igen

--- MudDemo.Client/wwwroot/js/scene.js
+++ MudDemo.Client/wwwroot/js/scene.js
@@ -26,7 +26,7 @@
         //renderer.render(scene, camera);
 
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 1000);
 
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth/2, window.innerHeight/2);
@@ -35,7 +35,8 @@
         /*document.body.appendChild(renderer.domElement);*/
 
         const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        var texture = new THREE.TextureLoader().load('sample-data/avatar.png');
+        const material = new THREE.MeshBasicMaterial({ map: texture });
         const cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
 
